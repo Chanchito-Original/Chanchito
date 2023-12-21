37552d0b8d7b59c460b73cbd670e92574c33b1ed
Parquiro <101524007+Parquiro@users.noreply.github.com>
2023-12-21 03:38:22 -0400

    Haciendolo parecer mas una pag vet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
 import React from 'react';
+import { FaCog, FaUser } from 'react-icons/fa'; // Importa los iconos necesarios
+import pigLogo from './pig-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bienvenido a la Clínica Veterinaria</h1>
-        <p>
-          Ofreciendo atención médica de calidad para tus mascotas.
-        </p>
-        <img
-          src="https://placekitten.com/200/200" // Puedes reemplazar esto con tu propio logo o imagen veterinaria
-          className="App-logo"
-          alt="logo"
-        />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-header-container">
+          <div className="App-logo-container">
+            {/* Agrega espacio entre el logo y el texto */}
+            <img src={pigLogo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Chanchito</h1>
+          </div>
+          <div className="App-header-buttons">
+            <button className="App-nav-button">
+              <FaCog /> Ajustes
+            </button>
+            <button className="App-nav-button">
+              <FaUser /> Perfil
+            </button>
+          </div>
+        </div>
       </header>
+
+      <main className="App-main">
+        <section className="App-section">
+          <h2>Solicitudes de Consultas</h2>
+          {/* Contenido de Solicitudes de Consultas */}
+        </section>
+
+        <section className="App-section">
+          <h2>Consultas en Curso</h2>
+          {/* Contenido de Consultas en Curso */}
+        </section>
+      </main>
     </div>
   );
 }
