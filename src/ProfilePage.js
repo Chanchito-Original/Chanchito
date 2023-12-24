2e6179ed10c5f4244b5a61e7229e608157c5e008
Parquiro <101524007+Parquiro@users.noreply.github.com>
2023-12-24 16:29:47 -0400

    Editing Profile

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,5 +1,4 @@
 // ProfilePage.js
-import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import {
   FaUser,
@@ -9,40 +8,64 @@ import {
   FaArrowLeft,
 } from "react-icons/fa";
 import pigLogo from "./img/pig-logo.png";
-import { Link } from "react-router-dom"; // Importa el componente Link para la navegación
+import { Link } from "react-router-dom";
 import "./css/ProfilePage.css";
 
+// Componente para las consultas
+const QueryBox = ({ title }) => (
+  <div className="profile-box">
+    <h3>{title}</h3>
+    {/* Contenido de la consulta */}
+  </div>
+);
+
+// Contenedor para las consultas
+const ProfileQueriesContainer = () => (
+  <div className="profile-queries-container">
+    <QueryBox title="Consulta 1" />
+    <QueryBox title="Consulta 2" />
+    {/* Agrega más consultas según sea necesario */}
+  </div>
+);
+
+// Componente principal de la página de perfil
 function ProfilePage() {
   return (
-    <div className="profile-container">
-      <div className="profile-header">
-        <img src={pigLogo} alt="Perfil" className="profile-avatar" />
-        <h1 className="profile-title">Nombre de Usuario</h1>
-        <Link to="/" className="back-button">
-          <FaArrowLeft />
-        </Link>
+    <div>
+      <div className="profile-container">
+        <div className="profile-header">
+          <Link to="/" className="back-button">
+            <FaArrowLeft />
+          </Link>
+          <img src={pigLogo} alt="Perfil" className="profile-avatar" />
+          <h1 className="profile-title">Nombre de Usuario</h1>
+        </div>
+
+        <div className="profile-details">
+          <div className="profile-detail">
+            <FaUser className="profile-icon" />
+            <span>Nombre: Nombre de Usuario</span>
+          </div>
+          <div className="profile-detail">
+            <FaEnvelope className="profile-icon" />
+            <span>Correo Electrónico: correo@ejemplo.com</span>
+          </div>
+          <div className="profile-detail">
+            <FaPhone className="profile-icon" />
+            <span>Teléfono: (123) 456-7890</span>
+          </div>
+        </div>
+
+        {/* Otros detalles del perfil, según sea necesario */}
+
+        <button className="profile-edit-button">
+          <FaEdit />
+          Editar Perfil
+        </button>
       </div>
 
-      <div className="profile-details">
-        <div className="profile-detail">
-          <FaUser className="profile-icon" />
-          <span>Nombre: Nombre de Usuario</span>
-        </div>
-        <div className="profile-detail">
-          <FaEnvelope className="profile-icon" />
-          <span>Correo Electrónico: correo@ejemplo.com</span>
-        </div>
-        <div className="profile-detail">
-          <FaPhone className="profile-icon" />
-          <span>Teléfono: (123) 456-7890</span>
-        </div>
-      </div>
-
-      {/* Otros detalles del perfil, según sea necesario */}
-
-      <button className="profile-edit-button">
-        <FaEdit />
-      </button>
+      {/* Nueva sección inferior fuera del container del perfil */}
+      <ProfileQueriesContainer />
     </div>
   );
 }
