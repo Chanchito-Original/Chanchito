b6b5ff8034498a9e8cb8436651e37c8f1f807a08
Parquiro <101524007+Parquiro@users.noreply.github.com>
2024-01-18 08:57:00 -0400

    Test en el servidor

--- src/server/server.js
+++ src/server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 4000; // Puedes cambiar el puerto según tus necesidades
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.send('Hola, mundo');
 });
 
 app.listen(port, () => {
